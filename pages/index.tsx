7f02a044a731d472f044ffc68a62d23470e1cbec
JoBenEtuk <etukjosiahbenjamin@gmail.com>
2023-06-26 21:15:36 +0100

    feat: faq

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import FrontLayout from '~/layout'
 import Hero from '~/components/home/hero'
+import Faq from '~/components/home/faq'
 
 const HomePage = () => {
 	return (
 		<FrontLayout page='home'>
 			<Hero />
+			<Faq />
 		</FrontLayout>
 	)
 }
---
+++ utils/mocks.ts
@@ -0,0 +1,23 @@
+/* eslint-disable quotes */
+export const faqs: { question: string; answer: string }[] = [
+	{
+		question: 'Is MonieVerse legal?',
+		answer:
+			'Monieverse is a legally registered company under the applicable laws and regulations of the jurisdictions in which we operate. We comply with all necessary legal requirements to ensure that our operations are legitimate and transparent.',
+	},
+	{
+		question: 'Is MonieVerse legal?',
+		answer:
+			'Monieverse is a legally registered company under the applicable laws and regulations of the jurisdictions in which we operate. We comply with all necessary legal requirements to ensure that our operations are legitimate and transparent.',
+	},
+	{
+		question: 'Is MonieVerse legal?',
+		answer:
+			'Monieverse is a legally registered company under the applicable laws and regulations of the jurisdictions in which we operate. We comply with all necessary legal requirements to ensure that our operations are legitimate and transparent.',
+	},
+	{
+		question: 'Is MonieVerse legal?',
+		answer:
+			'Monieverse is a legally registered company under the applicable laws and regulations of the jurisdictions in which we operate. We comply with all necessary legal requirements to ensure that our operations are legitimate and transparent.',
+	},
+]
