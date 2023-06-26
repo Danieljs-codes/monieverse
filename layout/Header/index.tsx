430494a9c359b129521c9d28bd5339fe9e8a82bc
JoBenEtuk <etukjosiahbenjamin@gmail.com>
2023-06-26 15:40:13 +0100

    feat: header scroll

--- layout/Header/index.tsx
+++ layout/Header/index.tsx
@@ -1,5 +1,8 @@
 import { Image, Link } from '~/shared'
 import s from './header.module.scss'
+import { useCallback, useRef } from 'react'
+import { useEventListener } from '@studio-lumio/hooks'
+import gsap from 'gsap'
 
 const Header = () => {
 	type ILinks = { href: string; name: string }
@@ -18,8 +21,38 @@ const Header = () => {
 		},
 	]
 
+	const lastScrollTop = useRef<number>(0)
+	const header = useRef<HTMLHeadingElement>(null)
+
+	const onScroll = useCallback(() => {
+		const duration = 0.3
+		const delay = 0.15
+		const ease = 'linear'
+		let st = window.scrollY || document.documentElement.scrollTop
+
+		if (st > lastScrollTop.current && window.scrollY > 100) {
+			gsap.to(header.current, {
+				y: '-200px',
+				scaleX: 1.1,
+				duration,
+				ease,
+				delay,
+			})
+		} else if (st < lastScrollTop.current) {
+			gsap.to(header.current, {
+				y: '0',
+				scaleX: 1,
+				duration,
+				ease,
+			})
+		}
+		lastScrollTop.current = st <= 0 ? 0 : st
+	}, [])
+
+	useEventListener('scroll', onScroll)
+
 	return (
-		<header className={s.header}>
+		<header ref={header} className={s.header}>
 			<div className={s['header-left']}>
 				<Link href='/'>
 					<Image alt='monie verse' width={183.37} height={32} src='/logo-text.svg' />
