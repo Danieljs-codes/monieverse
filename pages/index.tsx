4077c62b25457d4c0619bace6ad2a94daaccb622
JoBenEtuk <etukjosiahbenjamin@gmail.com>
2023-06-27 10:01:32 +0100

    feat: expand top

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import FrontLayout from '~/layout'
 import Hero from '~/components/home/hero'
 import Faq from '~/components/home/faq'
+import Expand from '~/components/home/expand'
 
 const HomePage = () => {
 	return (
 		<FrontLayout page='home'>
 			<Hero />
+			<Expand />
 			<Faq />
 		</FrontLayout>
 	)
