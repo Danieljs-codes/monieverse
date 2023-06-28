c2aa20b12f673031b42f5325f975cd514cb6d042
JoBenEtuk <etukjosiahbenjamin@gmail.com>
2023-06-28 20:13:17 +0100

    chore: add global

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import Hero from '~/components/home/hero'
 import Faq from '~/components/home/faq'
 import Expand from '~/components/home/expand'
 import Create from '~/components/home/create'
+import Global from '~/components/home/global'
 
 const HomePage = () => {
 	return (
 		<FrontLayout page='home'>
 			<Hero />
 			<Create />
+			<Global />
 			<Expand />
 			<Faq />
 		</FrontLayout>
